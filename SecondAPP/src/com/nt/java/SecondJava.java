5aa2cecdebeb89517117c99b5fa9a4f556075d49
admin <admin@admin-PC>
2018-06-11 12:42:16 +0530

    ok

---
+++ SecondAPP/src/com/nt/java/SecondJava.java
@@ -0,0 +1,11 @@
+package com.nt.java;
+
+public class SecondJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second git hub");
+
+	}
+
+}
